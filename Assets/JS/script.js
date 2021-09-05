bc4e144d0ef73dc49d96d295e1bb9ba50d75ec0b
Samuel Oliveira <oliveira_samuel14@hotmail.com>
2021-09-05 17:56:40 -0300

    Quase tudo pronto

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-
+    //ESCONDE E MOSTRA O MENU HAMBURGUER
     var vpWidth = $(window).width();
 
     if(vpWidth <= 699) {
@@ -53,6 +53,64 @@ $(document).ready(function () {
     $('.header .icon').click(function() {
         $('.header .icon').toggleClass("close")
         $('#nav').toggle()
+    })
+
+    //ESCONDE E MOSTRA AS PERGUNTAS DA SEÇÃO AKS    
+    $('#tit1').click(function(){
+        $('#tit1').toggleClass('show')
+        $('#par1').toggle()
+    });
+
+    $('#tit2').click(function(){
+        $('#tit2').toggleClass('show')
+        $('#par2').toggle()
+    });
+
+    $('#tit3').click(function(){
+        $('#tit3').toggleClass('show')
+        $('#par3').toggle()
+    });
+
+    $('#tit4').click(function(){
+        $('#tit4').toggleClass('show')
+        $('#par4').toggle()
+    });
+
+    //MENSAGEM DE ERRO NA SEÇÃO DE CONTACT
+    var msgerror = document.getElementById('msg-error')
+    var imgerror = document.getElementById('img-error')
+    var email = document.getElementById('email')
+    email.addEventListener('change', function() {
+        if(email.value == ""){
+            msgerror.style.display = 'block'
+            msgerror.style.background = 'hsl(0, 94%, 66%)'
+            imgerror.style.display = 'block'
+        }else if(!email.checkValidity()){
+            msgerror.style.display = 'block'
+            msgerror.style.background = 'hsl(0, 94%, 66%)' 
+            imgerror.style.display = 'block'
+        }else{
+            msgerror.style.display = 'block'
+            msgerror.style.background = 'hsl(98, 66%, 57%)'
+            imgerror.style.display = 'none'
+        }
+    })
+
+
+    var btn = document.querySelector('#btn')
+    btn.addEventListener('click', function() {
+        if(email.value == ""){
+            msgerror.style.display = 'block'
+            imgerror.style.display = 'block'
+        }else if(!email.checkValidity()){
+            msgerror.style.display = 'block'
+            imgerror.style.display = 'block'
+        }else{
+            msgerror.style.display = 'block'
+            msgerror.style.background = 'hsl(98, 94%, 66%)'
+            imgerror.style.display = 'none'
+        }
+
     })
 
     // TIRA E COLOCA A SOMBRA NO MENU

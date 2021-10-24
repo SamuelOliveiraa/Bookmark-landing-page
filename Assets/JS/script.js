5c14d9c89b7a8cb62efc67b3cb25b817d8147649
Samuel Oliveira <oliveira_samuel14@hotmail.com>
2021-10-24 19:12:55 -0300

    Site 100% de vdd dessa vez

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,46 +1,31 @@
 $(document).ready(function () {
     //ESCONDE E MOSTRA O MENU HAMBURGUER
     var vpWidth = $(window).width();
-
-    if(vpWidth <= 699) {
-        $('.header .icon').show()
-        $('#nav').hide()
-       
-        if($('.header .icon').hasClass('close')){
-            $('.header .icon').removeClass('close')
-            $('#nav').show()
-        }
-
-        $('#nav li a').click(function() {
-            $('.header .icon').removeClass('close')
-            $('#nav').hide()
-        })
-    }else if(vpWidth >= 700 ){
-        $('.header .icon').hide()
-        $('#nav').show()
-
-        $('#nav li a').click(function() {
-            $('#nav').show()
-        })
-    }
+    toggleHamburguerMenu(vpWidth)
 
     $(window).resize(function() {
         vpWidth = $(window).width();
-
+        toggleHamburguerMenu(vpWidth)
+    })
+    
+    $('.header .icon').click(function() {
+        $('.header .icon').toggleClass("close")
+        $('#nav').toggle()
+    })
+    //ESCONDE E MOSTRA O MENU HAMBURGUER
+    function toggleHamburguerMenu(vpWidth){
         if(vpWidth <= 699) {
             $('.header .icon').show()
             $('#nav').hide()
-           
             if($('.header .icon').hasClass('close')){
                 $('.header .icon').removeClass('close')
                 $('#nav').show()
             }
-    
             $('#nav li a').click(function() {
                 $('.header .icon').removeClass('close')
                 $('#nav').hide()
             })
-        }else if(vpWidth > 700 ){
+        }else if(vpWidth >= 700 ){
             $('.header .icon').hide()
             $('#nav').show()
     
@@ -48,34 +33,35 @@ $(document).ready(function () {
                 $('#nav').show()
             })
         }
-    })
-    
-    $('.header .icon').click(function() {
-        $('.header .icon').toggleClass("close")
-        $('#nav').toggle()
-    })
-
-    //ESCONDE E MOSTRA AS PERGUNTAS DA SEÇÃO AKS    
+    }
+    //ESCONDE E MOSTRA AS PERGUNTAS DA SEÇÃO AKS  
+    function toggleAsks(element, tit, height) {
+        if($(element).css('opacity') == 0){
+            $(tit).toggleClass('show')
+            $(element).css('height', height)
+            $(element).css('margin-top', '1.5rem')
+            $(element).css('opacity', '1')
+            $(element).css('z-index', '1')
+        }else{
+            $(tit).toggleClass('show')
+            $(element).css('height', '0')
+            $(element).css('margin-top', '0')
+            $(element).css('opacity', '0')
+            $(element).css('z-index', '-11')
+        }
+    }  
     $('#tit1').click(function(){
-        $('#tit1').toggleClass('show')
-        $('#par1').toggle()
+        toggleAsks('#par1', '#tit1', '57.6px')
     });
-
     $('#tit2').click(function(){
-        $('#tit2').toggleClass('show')
-        $('#par2').toggle()
+        toggleAsks('#par2', '#tit2', '134.4px')
     });
-
     $('#tit3').click(function(){
-        $('#tit3').toggleClass('show')
-        $('#par3').toggle()
+        toggleAsks('#par3', '#tit3', '96px')
     });
-
     $('#tit4').click(function(){
-        $('#tit4').toggleClass('show')
-        $('#par4').toggle()
+        toggleAsks('#par4', '#tit4', '57.6px')
     });
-
     //MENSAGEM DE ERRO NA SEÇÃO DE CONTACT
     var msgerror = document.getElementById('msg-error')
     var imgerror = document.getElementById('img-error')
@@ -113,70 +99,36 @@ $(document).ready(function () {
     })
 
     //ROLAGEM DE SEÇÃO NA PARTE DOS FEATURES
+    function toggleSection(elementAddClick, elementRemove1, elementRemove2, height, src, h2, p) {
+        $(elementAddClick).addClass('click')
+        $(elementRemove1).removeClass('click')
+        $(elementRemove2).removeClass('click')
+
+        $('.img-container').height(height)
+        $('#img-select').attr('src', src)
+        $('#h2-select').text(h2)
+        $('#p-select').text(p)
+    }
     $('#select1').click(function() {
-        $('#select1').addClass('click')
-        $('#select2').removeClass('click')
-        $('#select3').removeClass('click')
+        let p = 'Organize your bookmarks however you like. Our simple drag-and-drop interface gives you complete control over how you manage your favourite sites.'
 
-        
-        $('.img-container').height('300px')
-
-        $('#img-select').attr('src', 'Assets/images/illustration-features-tab-1.svg')
-
-        $('#h2-select').text('Bookmark in one click')
-
-        $('#p-select').text('Organize your bookmarks however you like. Our simple drag-and-drop interface gives you complete control over how you manage your favourite sites.')
-
+        toggleSection('#select1','#select2', '#select3','300px','Assets/images/illustration-features-tab-1.svg', 'Bookmark in one click', p)
     })
 
     $('#select2').click(function() {
-        $('#select2').addClass('click')
-        $('#select1').removeClass('click')
-        $('#select3').removeClass('click')
+        let p = 'Our powerful search feature will help you find saved sites in no time at all.No need to trawl through all of your bookmarks.'
 
-        $('.img-container').height('350px')
-
-        $('#img-select').attr('src', 'Assets/images/illustration-features-tab-2.svg')
-
-        $('#h2-select').text('Intelligent search')
-
-        $('#p-select').text('Our powerful search feature will help you find saved sites in no time at all.No need to trawl through all of your bookmarks.')
+        toggleSection('#select2','#select3', '#select1','350px','Assets/images/illustration-features-tab-2.svg', 'Intelligent search', p)
     })
 
     $('#select3').click(function() {
-        $('#select3').addClass('click')
-        $('#select2').removeClass('click')
-        $('#select1').removeClass('click')
-        
-        $('.img-container').height('350px')
+        let p = 'Easily share your bookmarks and collections with others. Create a shareable link that you can send at the click of a button.'
 
-        $('#img-select').attr('src', 'Assets/images/illustration-features-tab-3.svg')
-
-        $('#h2-select').text('Share your bookmarks')
-
-        $('#p-select').text('Easily share your bookmarks and collections with others. Create a shareable link that you can send at the click of a button.')
+        toggleSection('#select3','#select2', '#select1','350px','Assets/images/illustration-features-tab-3.svg', 'Share your bookmarks', p)
     })
-
-    //HOVER DO BOTÃO FIREFOX E CHROME
-
-    hover($('.firefox'), $('.chrome'))
-    hover($('.chrome'),$('.firefox'))
-
-    function hover(c, f){
-        $(c).hover(function() {
-            $(c).css({'background': 'hsl(231, 69%, 60%)',
-                     'color': 'white'})
-
-            $(f).css({'background-color':'white',
-                    'color': 'hsl(231, 69%, 60%)',
-                    'border': '1px solid hsl(231, 69%, 60%)'})
-        })
-    }
-
     // TIRA E COLOCA A SOMBRA NO MENU
     const header = document.querySelector('#header')
     const navHeight = header.offsetHeight
-
     window.addEventListener('scroll', function() {
         if(window.scrollY >= navHeight){
             header.classList.add('scroll')
@@ -184,7 +136,6 @@ $(document).ready(function () {
             header.classList.remove('scroll')
         }
     })
-
     /* ROLAGEM SUAVE */
     $('a[href^="#"]').on('click', function(e) {
         e.preventDefault();
@@ -196,11 +147,18 @@ $(document).ready(function () {
         }, 500);
     });
 })
+//HOVER DO BOTÃO FIREFOX E CHROME
 
-//HOVER DO BOTÃO BOX ICONS
-function hover2(e) {
-    $(e).attr('color', 'orange')
-}
-function out2(e) {
-    $(e).attr('color', 'white')
+hover($('.firefox'), $('.chrome'))
+hover($('.chrome'),$('.firefox'))
+
+function hover(c, f){
+    $(c).hover(function() {
+        $(c).css({'background': 'hsl(231, 69%, 60%)',
+                 'color': 'white'})
+
+        $(f).css({'background-color':'white',
+                'color': 'hsl(231, 69%, 60%)',
+                'border': '1px solid hsl(231, 69%, 60%)'})
+    })
 }
